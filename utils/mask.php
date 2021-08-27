adf73333fea361378a42ba633631522395ffd37d
WesleyMeneghini <wesley.meneghini@outlook.com.br>
2021-08-27 00:16:18 -0300

    Criando mascara para alguns campos

---
+++ utils/mask.php
@@ -0,0 +1,46 @@
+<?php
+
+
+function Mask($tipoMask, $str)
+{
+
+    if (strlen($tipoMask) == 10 && ($tipoMask == "telefone" || $tipoMask == "celular")) {
+        $tipoMask = "telefone";
+    } elseif (strlen($tipoMask) == 11 && ($tipoMask == "telefone" || $tipoMask == "celular")) {
+        $tipoMask = "celular";
+    }
+
+    switch ($tipoMask) {
+        case "telefone":
+            $mask = "(##) ####-####";
+            break;
+        case "celular":
+            $mask = "(##) #####-####";
+            break;
+        case "cnpj":
+            $mask = "##.###.###/####-##";
+            break;
+        case "cpf":
+            $mask = "###.###.###-##";
+            break;
+        case "cep":
+            $mask = "#####-###";
+            break;
+        default:
+            $mask = "";
+    }
+
+    if ($mask == "") {
+        return null;
+    } else {
+
+        echo 
+        $str = str_replace(" ", "", $str);
+
+        for ($i = 0; $i < strlen($str); $i++) {
+            $mask[strpos($mask, "#")] = $str[$i];
+        }
+
+        return $mask;
+    }
+}
